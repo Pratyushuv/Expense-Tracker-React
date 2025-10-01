4292f3fd11f60dc91af15059b76d5738803cf74e
pratyush <pratyushunnithan28@gmail.com>
2025-10-01 14:51:17 +0530

    Expense tracker c3

--- src/ExpenseList.jsx
+++ src/ExpenseList.jsx
@@ -2,7 +2,7 @@ import ExpenseItem from "./ExpenseItem";
 
 export default function ExpenseList({ expense }) {
   return (
-    <div className="expense-list  overflow-y-auto max-h-85 scrollbar-hide border-8 p-5 border-amber-600">
+    <div className="expense-list  overflow-y-auto max-h-85 scrollbar-hide border-8 p-5 border-amber-600 flex flex-col gap-5">
       {expense.map((exp, i) => (
         <ExpenseItem key={i} exp={exp} index={i + 1} />
       ))}
